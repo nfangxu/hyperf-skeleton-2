9c5c1af0e13a57b86480848ccc66cc5a1286c445
nfangxu <nfangxu@gmail.com>
2020-08-17 11:06:25 +0800

    test

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -11,16 +11,54 @@ declare(strict_types=1);
  */
 namespace App\Controller;
 
+use App\Model\User;
+use Fx\HyperfHttpAuth\Contract\HttpAuthContract;
+use Hyperf\Di\Annotation\Inject;
+
 class IndexController extends AbstractController
 {
+    /**
+     * @Inject()
+     * @var HttpAuthContract
+     */
+    protected $auth;
+
     public function index()
     {
-        $user = $this->request->input('user', 'Hyperf');
-        $method = $this->request->getMethod();
+        return $this->data();
+    }
 
+    /**
+     * 登录
+     */
+    public function login()
+    {
+        /** 方式 1 */
+        // 等价于 auth()->login(User::first());
+        $this->auth->login(User::first());
+
+        /** 方式 2 */
+        // 等价于 auth()->attempt(['email' => 'xxx', 'password' => '123456']);
+        $this->auth->attempt(['email' => 'xxx', 'password' => '123456']);
+
+        return $this->data();
+    }
+
+    /**
+     * 登出
+     */
+    public function logout()
+    {
+        // 等价于 auth()->logout();
+        $this->auth->logout();
+        return $this->data();
+    }
+
+    protected function data()
+    {
         return [
-            'method' => $method,
-            'message' => "Hello {$user}.",
+            'user' => auth()->user(),
+            'is_login' => auth()->check(),
         ];
     }
 }
